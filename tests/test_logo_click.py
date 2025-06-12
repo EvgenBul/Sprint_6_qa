cb0b557fdbc297cddd1e954fd7fe7a5bbde32a16
Evgenii Bulychev <zhindozz@yandex.ru>
2025-06-12 16:45:56 +0300

    POM: Инкапсулировал локаторы в test_logo_click.py

--- tests/test_logo_click.py
+++ tests/test_logo_click.py
@@ -1,36 +1,23 @@
 import allure
-from locators.main_page_locators import MainPageLocators
+from conftest import driver
 from pages.main_page import MainPage
-from selenium.webdriver.support.ui import WebDriverWait
 
-class TestLogo:
-    @allure.title('Проверка перехода на главную страницу сервиса при клике на лого "Самокат" в шапке')
-    def test_logo_main_page(self, driver):
+
+class TestLogoYandex:
+    @allure.title('Переход на главную страницу при нажатии на логотип "Самокат"')
+    def test_logo_redirect_to_main_page(self, driver):
         main_page = MainPage(driver)
-        main_page.wait_visibility_of_order_button_in_header()
-        main_page.click_on_order_button_in_header()
-        main_page.wait_visibility_of_header_logo_scooter()
-        main_page.click_on_element(MainPageLocators.header_logo_scooter)
+        main_page.wait_visibility_order_button_in_header()
+        main_page.click_order_button_in_header()
+        main_page.wait_visibility_header_logo_scooter()
+        main_page.click_header_logo_scooter()
         main_page.wait_visibility_of_main_header()
         assert main_page.check_displaying_of_main_header()
 
-    @allure.title('Проверка перехода на страницу "Дзена" при клике на лого "Яндекс"')
-    @allure.title('Проверка перехода на страницу "Дзена" при клике на лого "Яндекс"')
-    def test_logo_dzen(self, driver):
+    @allure.title('Переход на страницу "Дзена" при нажатии на логотип "Яндекс"')
+    def test_logo_redirect_to_dzen(self, driver):
         main_page = MainPage(driver)
-
-        with allure.step('1. Открыть главную страницу'):
-            main_page.open()
-
-        with allure.step('2. Кликнуть на логотип Яндекс'):
-            main_page.wait_visibility_of_header_logo_yandex()
-            main_page.click_on_header_logo_yandex()
-
-        with allure.step('3. Переключиться и проверить заголовок'):
-            WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)
-            main_page.switch_to_next_tab()
-
-            WebDriverWait(driver, 10).until(
-                lambda d: d.title != '' and 'Дзен' in d.title,
-                message=f'Заголовок не содержит "Дзен". Текущий заголовок: {driver.title}'
-            )
+        main_page.wait_visibility_logo_yandex()
+        main_page.click_on_logo_yandex()
+        main_page.switch_to_next_tab()
+        assert 'Дзен' in main_page.get_page_title()
